7d8a10b22ac55b0e8b1148e70907525d1f5d182e
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-05-28 02:15:49 +0300

    get data from server, parse it and put it to the Collection by unique key (symbol)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,53 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, JSONException {
 
+        URL url = new URL("https://sandbox.iexapis.com/stable/ref-data/symbols?token=Tpk_ee567917a6b640bb8602834c9d30e571");
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        httpURLConnection.setRequestMethod("GET");
+        httpURLConnection.connect();
+
+        int responce = httpURLConnection.getResponseCode();
+        String inline = "";
+
+        if (responce != 200)
+            throw new RuntimeException("as" + responce);
+        else {
+            Scanner scanner = new Scanner(url.openStream());
+            while (scanner.hasNext()) {
+                inline += scanner.nextLine();
+
+            }
+            scanner.close();
+        }
+
+        JSONArray jsonArray = new JSONArray(inline);
+
+        List<String> stockName = new ArrayList<String>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String name = jsonObject.getString("symbol");
+            boolean isTrue = jsonObject.getBoolean("isEnabled");
+            if (isTrue) {
+                stockName.add(name);
+            }
+        }
+        Collections.sort(stockName);
+        System.out.println(stockName);
 
     }
-
 }
