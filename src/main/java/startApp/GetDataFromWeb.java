1899c496973f5329a283fcd00849eeedacf09080
Vladyslav94 <grigoren_vlad2009@yahoo.com>
2020-05-28 20:07:33 +0300

    now we can access current stock information by its exchange acronym using different threads and shared queue (getting only company name as trial)

--- src/main/java/startApp/GetDataFromWeb.java
+++ src/main/java/startApp/GetDataFromWeb.java
@@ -3,18 +3,23 @@ package startApp;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Service
-public class GetDataFromWeb {
-    public void getData() throws IOException, JSONException {
+public class GetDataFromWeb extends Thread {
+    static LinkedBlockingQueue<String> storageForEnabledStock =  new LinkedBlockingQueue<String>();
+
+
+
+    public void getStockThatEnabled() throws IOException, JSONException, InterruptedException {
+        //getting json data about stock from remote server, parsing and putting to shared queue
         URL url = new URL("https://sandbox.iexapis.com/stable/ref-data/symbols?token=Tpk_ee567917a6b640bb8602834c9d30e571");
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
         httpURLConnection.setRequestMethod("GET");
@@ -24,7 +29,7 @@ public class GetDataFromWeb {
         String inline = "";
 
         if (response != 200)
-            throw new RuntimeException("as" + response);
+            throw new RuntimeException("Error while parsing" + response);
         else {
             Scanner scanner = new Scanner(url.openStream());
             while (scanner.hasNext()) {
@@ -36,18 +41,48 @@ public class GetDataFromWeb {
 
         JSONArray jsonArray = new JSONArray(inline);
 
-        List<String> stockName = new ArrayList<String>();
-
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
             String name = jsonObject.getString("symbol");
             boolean isTrue = jsonObject.getBoolean("isEnabled");
             if (isTrue) {
-                stockName.add(name);
+                storageForEnabledStock.offer(name);
             }
         }
-        Collections.sort(stockName);
-        System.out.println(stockName);
+
     }
+
+
+    public void getDataAbout() throws InterruptedException, IOException, JSONException {
+        while (true) {
+            String stockToGet = storageForEnabledStock.take();
+            String linkForTakingDataAboutStock =
+                    String.format("https://sandbox.iexapis.com/stable/stock/%s/quote?token=Tpk_ee567917a6b640bb8602834c9d30e571", stockToGet);
+            URL url = new URL(linkForTakingDataAboutStock);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.connect();
+
+            int response = httpURLConnection.getResponseCode();
+            String inline = "";
+
+            if (response != 200)
+                throw new RuntimeException("Error while parsing" + response);
+            else {
+                Scanner scanner = new Scanner(url.openStream());
+                while (scanner.hasNext()) {
+                    inline += scanner.nextLine();
+
+                }
+                scanner.close();
+            }
+
+            JSONObject jsonObject = new JSONObject(inline);
+            String infoAboutStock = jsonObject.getString("companyName");
+            System.out.println(infoAboutStock);
+
+        }
+    }
+
 
 }
--- src/main/java/startApp/StartStockApp.java
+++ src/main/java/startApp/StartStockApp.java
@@ -1,21 +1,69 @@
 package startApp;
 
+
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.io.IOException;
+
 
 @SpringBootApplication
 public class StartStockApp implements CommandLineRunner {
+
     public static void main(String[] args) {
         SpringApplication.run(StartStockApp.class, args);
-
     }
+
+
 
     @Autowired
     private GetDataFromWeb getDataFromWeb;
 
+
     public void run(String... args) throws Exception {
-        getDataFromWeb.getData();
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getDataFromWeb.getStockThatEnabled();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread thread1 = new Thread(runnable1);
+
+        Runnable runnable2 = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    getDataFromWeb.getDataAbout();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        Thread thread2 = new Thread(runnable2);
+
+        thread1.start();
+        thread2.start();
+
     }
+
+
 }
